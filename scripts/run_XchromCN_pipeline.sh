65fc1d0a523163b6d89c1ed5788276951e879e50
Eisa Mahyari <eisamahyari@gmail.com>
2023-01-24 15:19:37 -0800

    Update run_XchromCN_pipeline.sh
    init2

--- scripts/run_XchromCN_pipeline.sh
+++ scripts/run_XchromCN_pipeline.sh
@@ -20,3 +20,13 @@ xhmm --bam_list ${BAM_DIR}/bamfile_list.txt --gc_model /usr/local/bin/XHMM-1.0.1
 
 ## Run XCCNA
 #XCCNA -i ${BAM_DIR}/bamfile.bam -o /data/XchromCN_output/XCCNA_output
+
+#This script assumes that the BAM files are located in the /data/bamfiles directory, 
+#    and it creates a new directory called /data/XchromCN_output to store the output files.
+
+#It runs each of the bioinformatics tools one after the other: XHMM, cn.MOPS, XCNV, CopyNumber and XCCNA. 
+#Each tool is run using the appropriate command-line options and input/output files. 
+#In the case of CopyNumber, it's run using Rscript and it calls the CopyNumber package to run the pipeline.
+
+#The script uses the bamfile_list.txt file to provide the list of BAM files to the pipeline. 
+#    It should contain the path to the BAM files, one per line.
